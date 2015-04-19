c711f91c7b8c0de6c19565b3dcc1b4a123f43bc8
mamoch <mamoch@gmx-topmail.de>
2015-04-19 23:00:13 +0200

    relocated legend; added png export

--- speedtest_plot.py
+++ speedtest_plot.py
@@ -14,10 +14,10 @@ data = np.loadtxt(statsFile, delimiter='\t', usecols=(0, 5, 6, 7, 8), \
                   converters={0: strpdate2num('%Y-%m-%dT%H:%M:%S')})
 
 fig = plt.figure()
-ax1 = fig.add_subplot(111)
+ax1 = fig.add_axes([0.13, 0.13, 0.68, 0.8])
 
-ax1.plot(data[:, 0], data[:, 3], marker='.', label='Download')
-ax1.plot(data[:, 0], data[:, 4], marker='.', label='Upload')
+ax1.plot(data[:, 0], data[:, 3], marker='.', label='down')
+ax1.plot(data[:, 0], data[:, 4], marker='.', label='up')
 
 autoDL = AutoDateLocator()
 autoDF = AutoDateFormatter(autoDL)
@@ -27,12 +27,13 @@ ax1.xaxis.set_major_formatter(autoDF)
 
 fig.autofmt_xdate()
 ax1.set_ylabel('speed (Mbit/s)')
-ax1.legend(loc=0)
+ax1.legend(loc='upper left', bbox_to_anchor=(1.02, 1), borderaxespad=0)
 
 #ax2 = ax1.twinx()
 #ax2.plot(data[:, 0], data[:, 2], color='r', marker='.', label='Latency')
 #ax2.set_ylabel('latency (ms)')
 
 plt.savefig('speedtest_stats.pdf')
+plt.savefig('speedtest_stats.png', dpi=300)
 
 plt.show()
